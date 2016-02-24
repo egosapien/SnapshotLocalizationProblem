1badf581bc6415e6cb4d6e474ea363c0f2a7b945
egosapien <kiwiflorida@gmail.com>
2016-02-24 15:05:32 +0100

    Updated to newer Snapshot including newer helper

--- fastlane/SnapshotHelper.swift
+++ fastlane/SnapshotHelper.swift
@@ -10,6 +10,7 @@ import Foundation
 import XCTest
 
 var deviceLanguage = ""
+var locale = ""
 
 @available(*, deprecated, message="use setupSnapshot: instead")
 func setLanguage(app: XCUIApplication) {
@@ -17,33 +18,51 @@ func setLanguage(app: XCUIApplication) {
 }
 
 func setupSnapshot(app: XCUIApplication) {
-    Snapshot.setLanguage(app)
-    Snapshot.setLaunchArguments(app)
+    Snapshot.setupSnapshot(app)
 }
 
-func snapshot(name: String, waitForLoadingIndicator: Bool = false) {
+func snapshot(name: String, waitForLoadingIndicator: Bool = true) {
     Snapshot.snapshot(name, waitForLoadingIndicator: waitForLoadingIndicator)
 }
 
 class Snapshot: NSObject {
-    
+
+    class func setupSnapshot(app: XCUIApplication) {
+        setLanguage(app)
+        setLocale(app)
+        setLaunchArguments(app)
+    }
+
     class func setLanguage(app: XCUIApplication) {
         let path = "/tmp/language.txt"
-        
+
         do {
-            let locale = try NSString(contentsOfFile: path, encoding: NSUTF8StringEncoding) as String
-            deviceLanguage = locale.substringToIndex(locale.startIndex.advancedBy(2, limit:locale.endIndex))
-            app.launchArguments += ["-AppleLanguages", "(\(deviceLanguage))", "-AppleLocale", "\"\(locale)\"", "-ui_testing"]
+            deviceLanguage = try NSString(contentsOfFile: path, encoding: NSUTF8StringEncoding) as String
+            app.launchArguments += ["-AppleLanguages", "(\(deviceLanguage))"]
         } catch {
             print("Couldn't detect/set language...")
         }
     }
-    
+
+    class func setLocale(app: XCUIApplication) {
+        let path = "tmp/locale.txt"
+
+        do {
+            locale = try NSString(contentsOfFile: path, encoding: NSUTF8StringEncoding) as String
+        } catch {
+            print("Couldn't detect/set locale...")
+        }
+        if locale.isEmpty {
+            locale = NSLocale(localeIdentifier: deviceLanguage).localeIdentifier
+        }
+        app.launchArguments += ["-AppleLocale", "\"\(locale)\""]
+    }
+
     class func setLaunchArguments(app: XCUIApplication) {
         let path = "/tmp/snapshot-launch_arguments.txt"
-        
-        app.launchArguments += ["-FASTLANE_SNAPSHOT", "YES"]
-        
+
+        app.launchArguments += ["-FASTLANE_SNAPSHOT", "YES", "-ui_testing"]
+
         do {
             let launchArguments = try NSString(contentsOfFile: path, encoding: NSUTF8StringEncoding) as String
             let regex = try NSRegularExpression(pattern: "(\\\".+?\\\"|\\S+)", options: [])
@@ -56,28 +75,34 @@ class Snapshot: NSObject {
             print("Couldn't detect/set launch_arguments...")
         }
     }
-    
-    class func snapshot(name: String, waitForLoadingIndicator: Bool = false) {
+
+    class func snapshot(name: String, waitForLoadingIndicator: Bool = true) {
         if waitForLoadingIndicator {
             waitForLoadingIndicatorToDisappear()
         }
-        
-        print("snapshot: \(name)") // more information about this, check out https://github.com/krausefx/snapshot
-        
+
+        print("snapshot: \(name)") // more information about this, check out https://github.com/fastlane/snapshot
+
         sleep(1) // Waiting for the animation to be finished (kind of)
         XCUIDevice.sharedDevice().orientation = .Unknown
     }
-    
+
     class func waitForLoadingIndicatorToDisappear() {
         let query = XCUIApplication().statusBars.childrenMatchingType(.Other).elementBoundByIndex(1).childrenMatchingType(.Other)
-        
-        while query.count > 4 {
+
+        while (0..<query.count).map({ query.elementBoundByIndex($0) }).contains({ $0.isLoadingIndicator }) {
             sleep(1)
-            print("Number of Elements in Status Bar: \(query.count)... waiting for status bar to disappear")
+            print("Waiting for loading indicator to disappear...")
         }
+    }
+}
+
+extension XCUIElement {
+    var isLoadingIndicator: Bool {
+        return self.frame.size == CGSize(width: 10, height: 20)
     }
 }
 
 // Please don't remove the lines below
 // They are used to detect outdated configuration files
-// SnapshotHelperVersion [[1.0]]
+// SnapshotHelperVersion [1.1]
